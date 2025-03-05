03db9a072f4be2adf3e5b4a18866e0030083881b
shunyaves <shunyaves@gmail.com>
2025-03-06 01:39:33 +0200

    Seat.java

---
+++ Lab_4/src/Seat.java
@@ -0,0 +1,34 @@
+// TODO стоимость каждого билета считается по ценовому коэффициенту фильма
+
+public class Seat 
+{
+    private int price;
+    private int taken;
+
+    public Seat()
+  {
+        this.price = 10;
+        this.taken = 0;
+    }
+    public Seat(int price)
+  {
+        this.price = price;
+        this.taken = 0;
+    }
+    public void set_price(int koef)
+  {
+        price *= koef;
+    }
+    public void take_seat()
+  {
+        taken = 1;
+    }
+    public int is_taken()
+  {
+        return taken;
+    }
+    public int get_price()
+  {
+        return price;
+    }
+}
